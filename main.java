73c863d8f6fa3b5be7055639021a3ed3babd679f
adharsh888 <adharshcs442@gmail.com>
2024-12-20 14:55:11 +0530

    Update PrimeNumbers.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 
 class PrimeNumbers 
 {
-public static void main(String[] args) 
+public static void main(String args[]) 
 {
 int limit = 100;
 System.out.println("Prime numbers between 1 and " + limit);
