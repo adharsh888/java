2dff3274c542683ed674ec5924163ddc562dd352
adharsh888 <adharshcs442@gmail.com>
2024-12-20 13:57:40 +0530

    Update main1.java

--- main.java
+++ main.java
@@ -3,27 +3,27 @@
     void eat();
 }
 
- class Dog implements Animal {
+class Dog implements Animal {
     public void makeSound() {
-        System.out.println("Woof! 60 Woof!");
+        System.out.println("Woof! Woof!");
     }
 
     public void eat() {
-        System.out.println("Dog barking.");
+        System.out.println("Dog is eating.");
     }
 }
 
- class Cat implements Animal {
+class Cat implements Animal {
     public void makeSound() {
         System.out.println("Meow! Meow!");
     }
 
     public void eat() {
-        System.out.println("Cat will eat.");
+        System.out.println("Cat is eating.");
     }
 }
 
- class Main {
+public class Main1 {
     public static void main(String[] args) {
         Animal dog = new Dog();
         Animal cat = new Cat();
